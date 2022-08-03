26c788887e601695779bbb7807692efab4f1d40c
Steven <ssteven075@gmail.com>
2022-08-03 18:11:02 +0700

    fix: render on mount

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -12,15 +12,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 	// When mounted on client, now we can show the UI
 	React.useEffect(() => setMounted(true), []);
 
-	if (!mounted) return null;
-
 	return (
 		<>
-			{theme === "light" ? (
-				<NextNProgress color="#191919" startPosition={0.3} stopDelayMs={200} height={2} showOnShallow={true} />
-			) : (
-				<NextNProgress color="#f0f0f0" startPosition={0.3} stopDelayMs={200} height={2} showOnShallow={true} />
-			)}
+			{mounted &&
+				(theme === "light" ? (
+					<NextNProgress color="#191919" startPosition={0.3} stopDelayMs={200} height={2} showOnShallow={true} />
+				) : (
+					<NextNProgress color="#f0f0f0" startPosition={0.3} stopDelayMs={200} height={2} showOnShallow={true} />
+				))}
 
 			<section className="page">
 				<Header />
