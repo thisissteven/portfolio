70b35324a3d67d174baeba97e4b86b10f73f9d97
Steven <ssteven075@gmail.com>
2022-08-07 22:00:24 +0700

    fix: render on mount

--- src/components/SortListBox.tsx
+++ src/components/SortListBox.tsx
@@ -14,27 +14,34 @@ export default function SortListBox({
 }) {
 	const { theme } = useTheme();
 
+	const [mounted, setMounted] = React.useState(false);
+
+	// When mounted on client, now we can show the UI
+	React.useEffect(() => setMounted(true), []);
+
 	return (
 		<div className="max-w-[250px] min-w-[200px] self-end">
 			<Listbox value={selected} onChange={setSelected}>
 				<div className="relative mt-1">
-					<Listbox.Button
-						className={`relative w-full cursor-default ${
-							theme === "dark" ? "bg-[#2f2f2f]" : "bg-[#e2e2e2]"
-						} py-2 pl-3 pr-10 text-left rounded-md shadow-md focus:outline-none sm:text-sm`}
-					>
-						<span className="flex items-center">
-							{selected === "Sort by name" ? (
-								<TbSortAscendingLetters className="mr-2 text-base" />
-							) : (
-								<FaHeartbeat className="mr-2 text-base" />
-							)}
-							<span className="font-semibold  text-base">{selected}</span>
-						</span>
-						<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-							<HiSelector className="h-5 w-5 text-primary" aria-hidden="true" />
-						</span>
-					</Listbox.Button>
+					{mounted && (
+						<Listbox.Button
+							className={`relative w-full cursor-default ${
+								theme === "dark" ? "bg-[#2f2f2f]" : "bg-[#e2e2e2]"
+							} py-2 pl-3 pr-10 text-left rounded-md shadow-md focus:outline-none sm:text-sm`}
+						>
+							<span className="flex items-center">
+								{selected === "Sort by name" ? (
+									<TbSortAscendingLetters className="mr-2 text-base" />
+								) : (
+									<FaHeartbeat className="mr-2 text-base" />
+								)}
+								<span className="font-semibold  text-base">{selected}</span>
+							</span>
+							<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+								<HiSelector className="h-5 w-5 text-primary" aria-hidden="true" />
+							</span>
+						</Listbox.Button>
+					)}
 					<Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
 						<Listbox.Options
 							className={`rounded-md whitespace-nowrap absolute mt-2 max-h-60 w-full overflow-auto py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm ${
